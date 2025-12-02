bf7585e25b909ec706cd1ccc4a539b67be5ecdaf
gabagoo-grimbo <gabagoogrimbo@gmail.com>
2025-12-02 15:20:44 -0600

    style changes

--- rooms/main/main.gd
+++ rooms/main/main.gd
@@ -1,10 +1,14 @@
 extends PanelContainer
 
-@onready var button_container: Container = get_node("MarginContainer/VBoxContainer")
-@onready var day_1_button: Button = button_container.get_node("Day1Button")
+@onready var day_1_button: Button = %Day1Button
+@onready var day_2_button: Button = %Day2Button
 
 func _ready() -> void:
 	day_1_button.pressed.connect(_on_day_1_button_pressed)
+	day_2_button.pressed.connect(_on_day_2_button_pressed)
+
+func _on_day_2_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://rooms/day_2/day_2.tscn")
 
 func _on_day_1_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://rooms/day_1/day_1.tscn")
